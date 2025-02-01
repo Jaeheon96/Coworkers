83431f0dcd7678f0d4a541129a81eda8de8262fb
Jaeheon96 <jinnavsi015@naver.com>
2025-02-01 16:37:19 +0900

    design: 셋업 헤더의 누락된 반응형 디자인 추가

--- src/components/@shared/UI/SetupHeader.tsx
+++ src/components/@shared/UI/SetupHeader.tsx
@@ -3,17 +3,19 @@ import Link from "next/link";
 
 export default function SetupHeader() {
   return (
-    <header className="fixed top-0 z-40 flex h-[3.75rem] w-full items-center justify-between border-b border-border-primary bg-background-secondary px-[22.5rem] max-md:px-6 max-sm:px-4">
-      <Link href="/">
-        <div className="flex items-center gap-0.5">
-          <div className="relative h-6 w-6 max-md:h-4 max-md:w-4">
-            <Image fill src="icons/icon-logo.svg" alt="로고" />
+    <header className="fixed top-0 z-30 flex h-[3.75rem] w-full justify-center border-b border-border-primary bg-background-secondary px-6 [&&]:max-sm:px-4">
+      <div className="flex h-full w-[75rem] items-center justify-between">
+        <Link href="/">
+          <div className="flex items-center gap-0.5">
+            <div className="relative h-6 w-6 [&&]:max-md:h-4 [&&]:max-md:w-4">
+              <Image fill src="icons/icon-logo.svg" alt="로고" />
+            </div>
+            <div className="relative h-8 w-[8.25rem] [&&]:max-md:h-5 [&&]:max-md:w-[5.25rem]">
+              <Image fill src="icons/icon-title.svg" alt="타이틀" />
+            </div>
           </div>
-          <div className="relative h-8 w-[8.25rem] max-md:h-5 max-md:w-[5.25rem]">
-            <Image fill src="icons/icon-title.svg" alt="타이틀" />
-          </div>
-        </div>
-      </Link>
+        </Link>
+      </div>
     </header>
   );
 }
