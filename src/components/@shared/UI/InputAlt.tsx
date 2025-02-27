d71c701a6e4bc46f434041c3942c61abaaa9fa8a
Jaeheon96 <jinnavsi015@naver.com>
2025-02-28 00:17:20 +0900

    fix: global 파일 롤백 및 input 컴포넌트 배포 페이지 오류 수정

--- src/components/@shared/UI/InputAlt.tsx
+++ src/components/@shared/UI/InputAlt.tsx
@@ -7,7 +7,7 @@ interface Props extends InputHTMLAttributes<HTMLInputElement> {
 
 export default function InputAlt({ className, isError, ...props }: Props) {
   const classnameCombined = twMerge(
-    `w-full h-12 rounded-xl ${isError ? "[&&]:border-status-danger" : "[&&]:border-border-primary"} [&&]:bg-background-secondary px-4 placeholder:text-text-default hover:border-interaction-hover focus:border-interaction-focus focus:ring-0 [&&]:max-sm:h-11`,
+    `w-full h-12 rounded-xl ${isError ? "[&&]:border-status-danger" : "[&&]:border-border-primary"} [&&]:bg-background-secondary px-4 placeholder:text-text-default [&&]:hover:border-interaction-hover [&&]:focus:border-interaction-focus [&&]:focus:ring-0 [&&]:max-sm:h-11`,
     className,
   );
 
