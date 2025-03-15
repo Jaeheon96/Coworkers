cff5cbe2849bde54e5cbb0150c570067252f91cc
Jaeheon96 <jinnavsi015@naver.com>
2025-03-15 14:40:25 +0900

    feat: 계정 설정 페이지 UI 구현

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -1,3 +1,72 @@
+import Button from "@/components/@shared/UI/Button";
+import InputAlt from "@/components/@shared/UI/InputAlt";
+import InputLabel from "@/components/@shared/UI/InputLabel";
+import ProfileImagePreview from "@/components/@shared/UI/ProfileImagePreview";
+import { useAuth } from "@/core/context/AuthProvider";
+import useImageUpload from "@/lib/hooks/useImageUpload";
+import Image from "next/image";
+
 export default function Account() {
-  return <div>계정 설정 페이지</div>;
+  const { user } = useAuth(true);
+  const {
+    fileInputValue,
+    handleFileInputChange,
+    imagePreview,
+    clearFileInput,
+  } = useImageUpload(user?.image);
+
+  return (
+    <main className="mx-auto mt-[6.25rem] flex max-w-[49.5rem] flex-col gap-6 px-6 [&&]:max-md:mt-[5.25rem] [&&]:max-sm:px-4">
+      <h1 className="text-text-xl font-bold text-text-primary">계정 설정</h1>
+      <form className="flex w-full flex-col gap-6">
+        <ProfileImagePreview
+          value={fileInputValue}
+          onInputChange={handleFileInputChange}
+          preview={imagePreview}
+          clearFile={clearFileInput}
+        >
+          <div className="relative h-16 w-16">
+            <Image
+              fill
+              src="/icons/icon-default_profile.svg"
+              alt="프로필 이미지"
+            />
+          </div>
+        </ProfileImagePreview>
+        <InputLabel label="이름">
+          <InputAlt />
+        </InputLabel>
+        <InputLabel label="이메일">
+          <InputAlt
+            className="cursor-default text-text-disabled [&&]:bg-background-tertiary [&&]:hover:border-border-primary [&&]:focus:border-border-primary"
+            readOnly
+          />
+        </InputLabel>
+        <InputLabel label="비밀번호">
+          <div className="relative w-full">
+            <InputAlt
+              className="cursor-default text-text-disabled [&&]:bg-background-tertiary [&&]:hover:border-border-primary [&&]:focus:border-border-primary"
+              type="password"
+              value="password"
+            />
+            <Button
+              className="absolute right-4 top-2 h-8 w-[4.625rem] text-sm font-semibold text-white [&&]:max-sm:top-1.5"
+              variant="solid"
+              size="large"
+            >
+              변경하기
+            </Button>
+          </div>
+        </InputLabel>
+      </form>
+      <div className="flex cursor-pointer items-center gap-2">
+        <div className="relative h-6 w-6">
+          <Image fill src="/icons/icon-secession.svg" alt="탈퇴" />
+        </div>
+        <span className="text-text-lg font-medium text-status-danger">
+          회원 탈퇴하기
+        </span>
+      </div>
+    </main>
+  );
 }
