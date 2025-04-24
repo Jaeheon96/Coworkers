3e218203581eb1b4b54430d21796d968d77237a3
Jaeheon96 <jinnavsi015@naver.com>
2025-04-24 09:22:57 +0900

    refactor: 팀 페이지의 TaskLists 컴포넌트 다이내믹 임포트

---
+++ src/components/PageComponents/team/TaskListSkeleton.tsx
@@ -0,0 +1,13 @@
+export default function TaskListSkeleton() {
+  return (
+    <div className="flex flex-col">
+      <div className="mb-4 h-10 animate-pulse rounded-xl bg-background-secondary" />
+      <div className="mb-4 h-10 animate-pulse rounded-xl bg-background-secondary" />
+      <div className="mb-4 h-10 animate-pulse rounded-xl bg-background-secondary" />
+      <div className="mb-4 h-10 animate-pulse rounded-xl bg-background-secondary" />
+      <div className="mb-4 h-10 animate-pulse rounded-xl bg-background-secondary" />
+      <div className="mb-4 h-10 animate-pulse rounded-xl bg-background-secondary" />
+      <div className="mb-4 h-10 animate-pulse rounded-xl bg-background-secondary" />
+    </div>
+  );
+}
