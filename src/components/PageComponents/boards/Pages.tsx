1404cebafeb3510fb8bd3021fea9f4621bc87f7d
Jaeheon96 <jinnavsi015@naver.com>
2025-05-18 13:16:27 +0900

    feat: 자유게시판 페이지네이션 기능 구현

--- src/components/PageComponents/boards/Pages.tsx
+++ src/components/PageComponents/boards/Pages.tsx
@@ -3,29 +3,49 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 
 export default function Pages() {
-  const { pages, lastPage } = useBoardsData();
-  const { query } = useRouter();
-  const { page }: { page?: number } = query;
+  const { pages, lastPage, pagesLength } = useBoardsData();
+  const { query, push } = useRouter();
+  const page = +(typeof query.page === "string" ? query.page : "1");
+
+  const pushPage = (targetPage: number) => {
+    push({
+      pathname: "/boards",
+      query: {
+        ...query,
+        page: targetPage,
+      },
+    });
+  };
 
   return (
     <ul className="flex items-center gap-1">
-      {page === 1 || !page ? null : (
-        <li className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-full transition-colors duration-100 hover:bg-background-secondary">
+      {pages[0] === 1 ? null : (
+        <li
+          className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-full transition-colors duration-100 hover:bg-background-secondary"
+          onClick={() => pushPage(pages[0] - pagesLength)}
+        >
           <div className="relative h-4 w-4">
             <Image fill src="/icons/icon-arrow_left.svg" alt="이전 페이지로" />
           </div>
         </li>
       )}
       {pages.map((p) => {
-        const pageClassName = `rounded-full flex justify-center items-center w-8 h-8 text-text-lg font-semibold ${(page ?? 1) === p ? "text-brand-primary cursor-default" : "text-text-primary cursor-pointer transition-colors duration-100 hover:bg-background-secondary"}`;
+        const pageClassName = `rounded-full flex justify-center items-center w-8 h-8 text-text-lg font-semibold ${page === p ? "text-brand-primary cursor-default" : "text-text-primary cursor-pointer transition-colors duration-100 hover:bg-background-secondary"}`;
         return (
-          <li key={p} className={pageClassName}>
+          <li
+            key={p}
+            className={pageClassName}
+            onClick={page === p ? undefined : () => pushPage(p)}
+          >
             {p}
           </li>
         );
       })}
-      {(page ?? 1) === lastPage ? null : (
-        <li className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-full transition-colors duration-100 hover:bg-background-secondary">
+      {pages.includes(lastPage) ? null : (
+        <li
+          className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-full transition-colors duration-100 hover:bg-background-secondary"
+          onClick={() => pushPage(pages[pagesLength - 1] + 1)}
+        >
           <div className="relative h-4 w-4">
             <Image fill src="/icons/icon-arrow_right.svg" alt="다음 페이지로" />
           </div>
