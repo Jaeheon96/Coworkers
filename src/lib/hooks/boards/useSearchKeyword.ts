58328bb0a284cec8b586444c4e9b5cccdb0bc7fb
Jaeheon96 <jinnavsi015@naver.com>
2025-07-18 13:48:45 +0900

    refactor: 자유게시판 검색 기능 커스텀 훅으로 분리

--- src/lib/hooks/boards/useSearchKeyword.ts
+++ src/lib/hooks/boards/useSearchKeyword.ts
@@ -1,13 +1,25 @@
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
 export default function useSearchKeyword() {
-  const { query, isReady } = useRouter();
+  const { query, isReady, push } = useRouter();
   const [keyword, setKeyword] = useState("");
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    push({
+      pathname: "/boards",
+      query: {
+        ...query,
+        page: 1,
+        keyword,
+      },
+    });
+  };
 
   useEffect(() => {
     if (typeof query.keyword === "string") setKeyword(query.keyword);
   }, [isReady]);
 
-  return { keyword, setKeyword };
+  return { keyword, setKeyword, handleSubmit };
 }
