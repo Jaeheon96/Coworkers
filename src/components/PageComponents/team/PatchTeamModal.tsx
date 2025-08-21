96f6c0fbfc22b982e796a6257c0b3e1b994ab7b2
Jaeheon96 <jinnavsi015@naver.com>
2025-08-21 13:06:53 +0900

    refactor: 팀 정보 수정 모달이 onClose를 Props로 받지 않고 직접 통제하도록 수정

--- src/components/PageComponents/team/PatchTeamModal.tsx
+++ src/components/PageComponents/team/PatchTeamModal.tsx
@@ -4,7 +4,6 @@ import modalNames from "@/lib/constants/modalNames";
 import TeamSubmitForm from "./TeamSubmitForm";
 
 interface Props {
-  onClose: () => void;
   submitCallback: () => void;
   formValues: {
     teamId: string;
@@ -13,16 +12,13 @@ interface Props {
   };
 }
 
-export default function PatchTeamModal({
-  onClose,
-  submitCallback,
-  formValues,
-}: Props) {
+export default function PatchTeamModal({ submitCallback, formValues }: Props) {
   const { patchTeamModalName } = modalNames;
   const isOpen = useModalStore((state) => state.modals[patchTeamModalName]);
+  const closeModal = useModalStore((state) => state.closeModal);
 
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
+    <Modal isOpen={isOpen} onClose={() => closeModal(patchTeamModalName)}>
       <div className="max-w-123 p-6 max-sm:p-0">
         <TeamSubmitForm submitCallback={submitCallback} {...formValues} />
       </div>
