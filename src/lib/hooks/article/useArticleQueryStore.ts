42ac7db0eb8160d5367864d087c509e7472f4f11
Jaeheon96 <jinnavsi015@naver.com>
2025-09-15 20:21:28 +0900

    refactor: 자유게시글 페이지의 useQuery가 데이터를 반환하도록 수정

--- src/lib/hooks/article/useArticleQueryStore.ts
+++ src/lib/hooks/article/useArticleQueryStore.ts
@@ -1,11 +1,25 @@
-import { ArticleResponse } from "@/core/dtos/boards/boards";
+import { useRouter } from "next/router";
 import { useQuery } from "@tanstack/react-query";
+import { useAuth } from "@/core/context/AuthProvider";
+import { ArticleResponse } from "@/core/dtos/boards/boards";
+import getArticle from "@/core/api/boards/getArticle";
 
 export default function useArticleQueryStore(article: ArticleResponse | null) {
-  useQuery({
+  const { query } = useRouter();
+  const id = query.id as string;
+  const { user } = useAuth();
+
+  const articleQuery = useQuery({
     queryKey: ["Article", article?.id],
-    queryFn: () => article,
+    queryFn: async () => {
+      if (!user) return article;
+      const res = await getArticle(id);
+
+      return res;
+    },
     staleTime: 0,
     gcTime: 5000,
   });
+
+  return articleQuery;
 }
