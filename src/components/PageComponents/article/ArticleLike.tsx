8b3260bdbd30ccb0736958972c0b931709452dc3
Jaeheon96 <jinnavsi015@naver.com>
2025-09-20 20:52:11 +0900

    refactor: 좋아요 아이콘이 로그인시 포인터 커서를 보여주도록 수정

--- src/components/PageComponents/article/ArticleLike.tsx
+++ src/components/PageComponents/article/ArticleLike.tsx
@@ -1,17 +1,22 @@
 import Image from "next/image";
 import { useArticleQuery } from "@/core/context/ArticleQueryProvider";
 import { ArticleResponse } from "@/core/dtos/boards/boards";
+import { useAuth } from "@/core/context/AuthProvider";
 
 interface Props {
   article: ArticleResponse;
 }
 
 export default function ArticleLike({ article }: Props) {
+  const { user } = useAuth();
   const { articleQueryData, isArticleQueryPending } = useArticleQuery();
+
   const { isLiked, likeCount } = articleQueryData ?? article;
 
+  const likeClassName = `flex items-center gap-1${user ? " cursor-pointer" : null}`;
+
   return (
-    <div className="flex items-center gap-1">
+    <div className={likeClassName}>
       {isArticleQueryPending ? (
         <div className="relative h-4 w-4 animate-spin">
           <Image fill src="/icons/icon-ongoing.svg" alt="좋아요 불러오는중" />
